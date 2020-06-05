38bf812801b7e00f3817fa1ba8fc22d9218229e4
TomKrepa <tom.krepa@ynov.com>
2020-06-05 05:58:22 -1200

    Ajout du panier

--- games/src/Controller/SiteController.php
+++ games/src/Controller/SiteController.php
@@ -11,6 +11,7 @@ use App\Repository\JeuxRepository;
 use App\Repository\CommentairesRepository;
 use App\Form\CommentType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class SiteController extends AbstractController
 {
@@ -32,14 +33,16 @@ class SiteController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function home() {
+    public function home()
+    {
         return $this->render('site/home.html.twig');
     }
 
     /**
      * @Route("/games/{id}", name="game_show")
      */
-    public function show(Jeux $jeu, Request $request, CommentairesRepository $repo, UserInterface $user) {
+    public function show(Jeux $jeu, Request $request, CommentairesRepository $repo, UserInterface $user)
+    {
 
         $comment = new Commentaires();
         $form = $this->createForm(CommentType::class, $comment);
@@ -49,12 +52,12 @@ class SiteController extends AbstractController
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $comment->setCreatedAt(new \DateTime())
-                    ->setJeux($jeu)
-                    ->setEmail($user->getUsername())
-                    ->setPseudo($user->getPseudo());
+                ->setJeux($jeu)
+                ->setEmail($user->getUsername())
+                ->setPseudo($user->getPseudo());
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($comment);
@@ -68,5 +71,71 @@ class SiteController extends AbstractController
             'commentaires' => $commentaires,
             'commentForm' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/panier", name="panier")
+     */
+    public function panier(SessionInterface $session, JeuxRepository $repo)
+    {
+
+        $panier = $session->get('panier', []);
+
+        $panierWithData = [];
+
+        foreach ($panier as $id => $quantity) {
+            $panierWithData[] = [
+                'jeux' => $repo->find($id),
+                'quantity' => $quantity
+            ];
+        }
+
+        $total = 0;
+
+        foreach ($panierWithData as $item) {
+            $totalItem = $item['jeux']->getPrix() * $item['quantity'];
+            $total += $totalItem;
+        }
+
+        return $this->render('site/panier.html.twig', [
+            'items' => $panierWithData,
+            'total' => $total
+        ]);
+    }
+
+    /**
+     * @Route("/panier/ajouter/{id}", name="panier_add")
+     */
+    public function ajouter($id, SessionInterface $session)
+    {
+
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute('games');
+    }
+
+    /**
+     * @Route("/panier/supprimer/{id}", name="panier_remove")
+     */
+    public function remove($id, SessionInterface $session)
+    {
+
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute('panier');
     }
 }
